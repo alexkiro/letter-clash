10cbf2bbf350d1ee69fe911c3f28b7488af03b3c
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2024-03-10 18:03:59 +0200

    Add new linter options

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,37 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:all",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting",
+    "plugin:perfectionist/recommended-natural",
+    "plugin:prettier/recommended",
   ],
   overrides: [
     {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'cypress/support/**/*.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
-    }
+      extends: ["plugin:cypress/recommended"],
+      files: ["cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}", "cypress/support/**/*.{js,ts,jsx,tsx}"],
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+  plugins: ["perfectionist"],
+  root: true,
+  rules: {
+    // Don't force capitalized comments
+    "capitalized-comments": ["off"],
+    // Enforce separate lines for multiline comments
+    "multiline-comment-style": ["error", "separate-lines"],
+    "no-magic-numbers": "off",
+    // Force separate var declaration
+    "one-var": ["error", "never"],
+    // Disable sorting rules, as we are using the perfectionist plugin that auto-fixes them.
+    "sort-imports": "off",
+    "sort-keys": "off",
+    "sort-vars": "off",
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+import { URL, fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
